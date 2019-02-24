b0ec5cb68ac3284e76ed4174bfbb3e74a6676ea3
russellostack <32462495+russellostack@users.noreply.github.com>
2019-02-24 13:06:07 -0500

    initialization, quiz created and coded into js, basic html made

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var questionArray = [
+    "How tall is the tallest Pyramid at Giza?",
+    "Whats the name of the tallest Pyramid?",
+    "What are The Pyramids made out of?",
+    "What were the Pyramids capped with after construction finished?",
+    "How may Pyramids are at Giza?",
+    "how tall is the shortest Pyramid at Giza?",
+    "When was the title of tallest man made structure taken from the Pyramid of Khufu?",
+    "What are the three Pyramids north of Khufu called?",
+    "What group of people mainly built The Pyramids?",
+    "Every Pyramid has a base that is a: "]
+var answerArray = [["481 Feet", "400 Feet", "140 Meters", "1000 feet"],["Khafre's Pyramid", "Khufu's Pyramid", "Menkaure's Pyramid", "Tutankhamun's Pyramid"],["Limestone", "Basalt", "Sandstone", "Granite"],["Gold and Basalt", "Gold and Limestone", "Bronze and Granite", "Granite and Gold"],["3", "2", "6", "10"],["50 Feet", "210 Feet", "106 Feet", "80 Feet"],["852 A.D.", "1835 A.D.", "204 B.C.", "1311 A.D."],["The Pyrmids of the Builders", "The Queen's Pyramids", "Tomb of Hemon", "The Great Sphinx"],["Priests", " Foreign Slaves", "Egyptian Farmers", "Aliens"],["Perfect Rectangle", "Perfect Triangle", "Perfect Square", "Perfect Circle"]]
+
+var correctArray=["481 Feet","Khufu's Pyramid","Granite","Gold and Limestone","6","106 Feet","1311 A.D.","The Queen's Pyramids","Egyptian Farmers","Perfectly Square"]
